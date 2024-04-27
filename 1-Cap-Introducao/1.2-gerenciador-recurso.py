0884a6e87cbb7a5700829e1549cec5de2d756028
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-04-27 08:58:14 -0300

    Created 1.2-Historia-SO.py

---
+++ 1-Cap-Introducao/1.2-gerenciador-recurso.py
@@ -0,0 +1,7 @@
+'''
+    Uma visão bottom-up sustenta que a função dos SO
+    seria o gerenciamento de recursos, 
+    mantendo um controle sobre quais programas estão usando qual, recurso,
+    e por quanto tempo, assim como mediar requisições conflitantes de difrentes programas e usuários.
+'''
+23/778
--- 1.2-gerenciador-recurso.py
+++
@@ -1,2 +0,0 @@
-file:///L:/Linear_/Base-Jump_05/Sistemas%20Operacionais%20Modernos%20-%20Tanenbaum%20-%204%20Edi%C3%A7%C3%A3o.pdf
-23/778
